2d1082397318954f1781047cdc8e132ca1562fa5
ivanovdale <dania-spb@mail.ru>
2023-12-22 15:34:35 +0300

    Initial commit

---
+++ mbf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mbf
+//
+//  Created by Daniil (work) on 22.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
